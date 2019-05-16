115c7164ff6053652108b7b1a07c8e37af3cf2bf
Connor Callahan <connorwcallahan@gmail.com>
2019-05-15 20:33:20 -0400

    issues with the footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,11 +9,13 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 import Navigation from "./navigation"
+import Footer from "./footer"
 import config from  "../data/config"
 import "../style/navigation.scss"
 import "../style/index.scss"
 import "../style/projects.scss"
 import "../style/contact.scss"
+import "../style/footer.scss"
 
 
 const Layout = ({ children }) => (
@@ -28,10 +30,11 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
+      <div className="app">
         <Navigation navLinks={config}/>
         <main>{children}</main>
-      </>
+        <Footer />
+      </div>
     )}
   />
 )
