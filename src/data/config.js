d45bc50011925d27fcfc8778ba8a0e60a135ef10
Connor Callahan <connorwcallahan@gmail.com>
2019-05-23 06:39:16 -0400

    typo fix

--- src/data/config.js
+++ src/data/config.js
@@ -9,7 +9,7 @@ const config = {
   dateFromFormat: 'YYYY-MM-DD',
   dateFormat: 'MMMM Do, YYYY',
   siteDescription:
-    'I am a Full Stack Web Developer who loves to building applications and collaborating with teams to solve challenging problems. Please take a minute to browse my projects, check out my blog or say hello! ',
+    'I am a Full Stack Web Developer who loves building applications and collaborating with teams to solve challenging problems. Please take a minute to browse my projects, check out my blog or say hello! ',
   siteRss: '/rss.xml',
   postDefaultCategoryID: 'Tech',
   userName: 'Connor',
