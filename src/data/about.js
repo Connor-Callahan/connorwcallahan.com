d0c540029aa34bb46424cdd885753a748d8bbce4
Connor Callahan <connorwcallahan@gmail.com>
2019-08-13 14:53:15 -0400

    netlify deploy

--- src/data/about.js
+++ src/data/about.js
@@ -2,7 +2,7 @@ import profile from '../images/profile.jpeg'
 
 
 const about = {
-    p1: "I love to help people solve problems. Working with others to build, create, and discover new solutions is the reason I get out of bed in the morning and a thrill I constantly seek.",
+    p1: "I love to help people solve problems.",
     p2: "My journey in code has been remarkable and I am continually blown away by the community and people I meet in this field. Whether you're someone looking to get started on a project with a freelance web developer, a developer seeking to collaborate, or anyone who would like to say hi, send me a message and let's get in touch.",
     image: profile,
   }
